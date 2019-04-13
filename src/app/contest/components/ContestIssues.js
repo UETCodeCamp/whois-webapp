599fa281438b63de3e390dedcec85d02bb495f82
quytm <minhquylt95@gmail.com>
2019-04-13 23:43:30 +0700

    contest-detail: add Created Date field

--- src/app/contest/components/ContestIssues.js
+++ src/app/contest/components/ContestIssues.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {getListIssues} from "../../../services/apis/ContestAPIServices"
 import ReactTooltip from 'react-tooltip'
+import moment from "moment"
 
 class ContestIssues extends Component {
     state = {
@@ -48,13 +49,14 @@ class ContestIssues extends Component {
                         <tr>
                             <th scope="col">#</th>
                             <th scope="col">Username</th>
-                            <th scope="col">FullName</th>
+                            <th scope="col">Full Name</th>
                             <th scope="col">Status</th>
+                            <th scope="col">Created date</th>
                         </tr>
                         </thead>
                         <tbody>
                         {issues.map((issue, index) => {
-                            const {_id, github_id, camper, title, status, message} = issue
+                            const {_id, github_id, camper, title, status, message, created} = issue
                             const {username} = Object.assign({}, camper)
 
                             return (
@@ -68,6 +70,7 @@ class ContestIssues extends Component {
                                         </div>
                                         <ReactTooltip id='enrich' getContent={(dataTip) => dataTip}/>
                                     </td>
+                                    <td>{moment(created).format('DD/MM/YYYY')}</td>
                                 </tr>
                             )
                         })}
